3727fd41ddb4fd3f8cc1193e9ab6b99f8cd90cd7
Vitor Brito <vitoraraujo1694@gmail.com>
2025-06-14 20:54:21 -0300

    feat: Criar funcao utilitaria para identificar iniciais do nome

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getInitials(fullName: string): string {
+  const names = fullName
+    .trim()
+    .split(" ")
+    .filter((name) => name.length > 0);
+  if (names.length === 0) return "";
+  if (names.length === 1) return names[0].charAt(0).toUpperCase();
+  return (names[0].charAt(0) + names[names.length - 1].charAt(0)).toUpperCase();
 }
