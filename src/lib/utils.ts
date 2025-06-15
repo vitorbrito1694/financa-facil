a02e1570f9c62faab166fbd59e5541fc24a67a37
Vitor Brito <vitoraraujo1694@gmail.com>
2025-06-14 22:31:24 -0300

    feat: Criar funcao para extrair primeiro e ultimo nome de nome completo

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -14,3 +14,13 @@ export function getInitials(fullName: string): string {
   if (names.length === 1) return names[0].charAt(0).toUpperCase();
   return (names[0].charAt(0) + names[names.length - 1].charAt(0)).toUpperCase();
 }
+
+export function getFirstAndLastName(fullName: string): string {
+  const names = fullName
+    .trim()
+    .split(" ")
+    .filter((name) => name.length > 0);
+  if (names.length === 0) return "";
+  if (names.length === 1) return names[0];
+  return names[0] + " " + names[names.length - 1];
+}
