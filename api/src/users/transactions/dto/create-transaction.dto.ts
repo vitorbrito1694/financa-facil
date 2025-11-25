d34e1d7cd4475d8bce22688e737284dde924e1c6
Vitor Brito <vitoraraujo1694@gmail.com>
2025-11-24 23:00:59 -0300

    feat: add class and frequency to transaction entity and its creation DTO.

--- api/src/users/transactions/dto/create-transaction.dto.ts
+++ api/src/users/transactions/dto/create-transaction.dto.ts
@@ -6,37 +6,66 @@ import {
   IsString,
   IsUUID,
   IsDateString,
+  IsBoolean,
 } from 'class-validator';
-import { TransactionType } from '../transaction.entity';
+import {
+  TransactionClass,
+  TransactionFrequency,
+  TransactionType,
+} from '../transaction.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTransactionDto {
+  @ApiProperty({ example: 100 })
   @IsNumber()
   @IsNotEmpty()
   amount: number;
 
+  @ApiProperty({ example: '2025-11-24' })
   @IsDateString()
   @IsNotEmpty()
   date: string;
 
+  @ApiProperty({ example: '2025-11-24' })
   @IsDateString()
   @IsNotEmpty()
   paymentAt: string;
 
+  @ApiProperty({ example: 'EXPENSE' })
   @IsEnum(TransactionType)
   @IsNotEmpty()
   type: TransactionType;
 
+  @ApiProperty({ example: 'FIXED' })
+  @IsEnum(TransactionFrequency)
+  @IsNotEmpty()
+  frequency: TransactionFrequency;
+
+  @ApiProperty({ example: 'ESSENTIAL' })
+  @IsEnum(TransactionClass)
+  @IsNotEmpty()
+  class: TransactionClass;
+
+  @ApiProperty({ example: false })
+  @IsBoolean()
+  @IsOptional()
+  note?: boolean;
+
+  @ApiProperty({ example: 'Descrição da transação' })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({ example: 'uuid-do-metodo-de-pagamento' })
   @IsUUID()
   payment_method_id: string;
 
+  @ApiProperty({ example: 1 })
   @IsNumber()
   @IsOptional()
   installmentCount?: number;
 
+  @ApiProperty({ example: 1 })
   @IsNumber()
   @IsOptional()
   installmentNumber?: number;
--- api/src/users/transactions/transaction.entity.ts
+++ api/src/users/transactions/transaction.entity.ts
@@ -13,10 +13,21 @@ import { PaymentMethod } from '../payment-method/payment-method.entity';
 export enum TransactionType {
   INCOME = 'INCOME',
   EXPENSE = 'EXPENSE',
-  TRANSFER = 'TRANSFER',
+  INVESTMENT = 'INVESTMENT',
 }
 
-// TODO - Preciso inserir as classificacoes da transacao. FREQUENCIA, CLASSE e TIPO
+export enum TransactionFrequency {
+  FIXED = 'FIXED',
+  RECURRING = 'RECURRING',
+  PROBABLE = 'PROBABLE',
+}
+
+export enum TransactionClass {
+  ESSENTIAL = 'ESSENTIAL',
+  NOT_ESSENTIAL = 'NOT_ESSENTIAL',
+  OPTIONAL = 'OPTIONAL',
+  SAVING = 'SAVING',
+}
 
 @Entity('transactions')
 export class Transaction {
@@ -26,29 +37,66 @@ export class Transaction {
   @Column({ type: 'uuid' })
   user_id: string;
 
-  @Column({ type: 'uuid', nullable: false })
+  @Column({ type: 'uuid', nullable: false, comment: 'Metodo de pagamento' })
   payment_method_id: string;
 
-  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  @Column({
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+    comment: 'Valor da transacao',
+  })
   amount: number;
 
-  @Column({ type: 'timestamptz' })
+  @Column({ type: 'timestamptz', comment: 'Data da transacao' })
   date: Date;
 
-  @Column({ type: 'timestamptz' })
+  @Column({ type: 'timestamptz', comment: 'Data do pagamento' })
   paymentAt: Date;
 
-  @Column({ type: 'enum', enum: TransactionType })
+  @Column({ type: 'enum', enum: TransactionType, comment: 'Tipo da transacao' })
   type: TransactionType;
+
+  @Column({
+    type: 'enum',
+    enum: TransactionFrequency,
+    comment: 'Frequencia da transacao',
+  })
+  frequency: TransactionFrequency;
+
+  @Column({
+    type: 'enum',
+    enum: TransactionClass,
+    comment: 'Classe da transacao',
+  })
+  class: TransactionClass;
 
   @Column({ type: 'varchar', nullable: true })
   description: string;
 
-  @Column({ type: 'int', default: 1, nullable: false })
+  @Column({
+    type: 'int',
+    default: 1,
+    nullable: false,
+    comment: 'Quantidade de parcelas',
+  })
   installmentCount: number;
 
-  @Column({ type: 'int', default: 1, nullable: false })
+  @Column({
+    type: 'int',
+    default: 1,
+    nullable: false,
+    comment: 'Numero da parcela',
+  })
   installmentNumber: number;
+
+  @Column({
+    type: 'boolean',
+    default: false,
+    nullable: false,
+    comment: 'Se a transacao for apenas um apontamento',
+  })
+  note: boolean;
 
   @ManyToOne(() => User, (user) => user.transactions, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'user_id' })
